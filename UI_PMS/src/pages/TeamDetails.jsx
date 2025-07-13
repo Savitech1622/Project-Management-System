4d435a693717dcc6db9c471315616fbdd47074e7
Savitech .io <savitech1622@gmail.com>
2025-07-13 06:15:54 +0000

    Team Detail Page Header Initial

--- UI_PMS/src/pages/TeamDetails.jsx
+++ UI_PMS/src/pages/TeamDetails.jsx
@@ -30,57 +30,27 @@ const TeamDetails = () => {
     // const {userid, refresh, setRefresh} = useMyStore();
     const {userid} = useMyStore();
 
+    let [projectDetails, setProjectDetails] = useState({});
+    let [memberDetails, setMemberDetails] = useState([]);
+
     useEffect(() => {
-      getTeamData();
+      getTeamDetailsData();
     }, [refresh]);
 
-    const createNewProject = async() => {
-      try 
-      {
-        setLoading(true);
-        
-        const { data, error } = await supabase
-          .from('t_sys_teams')
-          .insert({project_name: projectName, project_desc: '', org_id: orgId, created_by: userid, status: 1, modified_date: new Date().toISOString(), marks: 0})
-          .select()
-
-        if(error)
-        {
-          setLoading(false);
-          setPopup(false);
-          throw error;
-        }
-
-        if(data.length > 0)
-        {
-          setPopup(false);
-          setRefresh(() => refresh = refresh + 1);
-        }
-          
-      }
-      catch(ex)
-      {
-        console.log('Error Occured At createNewProject: '+ex.message);
-      }
-      finally
-      {
-        clearForm();
-        setLoading(false);
-      }
-    }
 
     const clearForm = () => {
       setProjectName('');
     }
 
-    const getTeamData = async() => {
+    const getTeamDetailsData = async() => {
       try
       {
         setLoading(true);
 
         let { data: t_sys_teams, error } = await supabase
         .from('t_sys_teams')
-        .select('*').eq('org_id', orgId);
+        .select(`*, user_team: userteam(id, users)`)
+        .eq('org_id', orgId).eq('id', teamId);
 
         if(error)
         {
@@ -90,17 +60,48 @@ const TeamDetails = () => {
 
         if(t_sys_teams.length > 0)
         {
-          setOrgList(t_sys_teams);
+          console.log(t_sys_teams)
+          const project = t_sys_teams[0];
+          const projectDetailObj = {
+            name: project.project_name,
+            status : project.status
+          }
+
+          const members = project?.user_team?.users;
+          const memberId = members.map((val, ind) => {
+            return (
+              val.userid
+            )
+          })
+
+          
+          let { data: t_sys_users, error: user_error } = await supabase
+            .from('t_sys_users')
+            .select('*').in('id', memberId);
+
+          if(user_error)
+            throw user_error;
+
+          if(t_sys_users.length > 0)
+          {
+            setMemberDetails(t_sys_users);
+          }
+          else
+          {
+            setMemberDetails([]);
+          }
+
+          setProjectDetails(projectDetailObj);
         }
         else
         {
-          setOrgList([]);
+          setProjectDetails({});
         }
 
       }
       catch(ex)
       {
-        console.log('Error Occured At getTeamData: '+ex.message);
+        console.log('Error Occured At getTeamDetailsData: '+ex.message);
       }
       finally
       {
@@ -116,8 +117,33 @@ const TeamDetails = () => {
               <div style={{width:'100%'}}>
 
                   <div style={{paddingInline:60, paddingTop:24}}>
-                    <h2 style={{fontSize:24}}>My Dashboard</h2>
+                    <div style={{display:'flex', gap:'8px', alignItems:'center'}}>
+                        <h2 style={{fontSize:24}}>{projectDetails?.name || ''}</h2>
+                        {projectDetails?.status == 1 && <p style={{fontSize:'12px', backgroundColor:'red', paddingInline:'6px', paddingBlock:'4px', color:'white', borderRadius:'12px', fontWeight:500}}>Not Started</p>}
+                        {projectDetails?.status == 2 && <p style={{fontSize:'12px', backgroundColor:'orange', paddingInline:'6px', paddingBlock:'4px', color:'white', borderRadius:'12px', fontWeight:500}}>In Progress</p>}
+                        {projectDetails?.status == 3 && <p style={{fontSize:'12px', backgroundColor:'green', paddingInline:'6px', paddingBlock:'4px', color:'white', borderRadius:'12px', fontWeight:500}}>Complete</p>}
+                    </div>
+                    <div style={{display:'flex', flexDirection:'column'}}>
+                      <h2 style={{fontSize:16, color:'#00000070', fontWeight:400, marginTop:'8px'}}>Members</h2>
+                      <div style={{display:'flex', gap:'8px', marginTop:'6px'}}>
+                        {
+                          memberDetails?.map((val, ind) => {
+                            return (
+                              <>
+                                <div>
+                                  <div style={{marginBlock:'4px', boxShadow:'0 1px 4px rgba(0, 0, 0, 0.3)', fontSize:'10px', color:'white', background:'purple', fontWeight:600, width:'30px', height:'30px', border:'2px solid #ffffff', borderRadius:'120', display:'flex', justifyContent:'center', alignItems:'center', borderRadius:'120px'}}>
+                                    {val.name.split(' ').length > 1 ? val.name.split(' ')[0][0].toUpperCase()+val.name.split(' ')[1][0].toUpperCase(): val.name[0].toUpperCase()}
+                                  </div>
+                                  <h2 style={{fontSize:12, fontWeight:400, textAlign:'center'}}>{val.name.split(' ')[0]}</h2>
+                                </div>
+                              </>
+                            )
+                          })
+                        }
+                      </div>
+                    </div>
                   </div>
+                  
 
               </div>
             </div>
