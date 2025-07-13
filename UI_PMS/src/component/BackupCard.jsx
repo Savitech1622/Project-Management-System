08f321e12aeb85563412c2a7278167e3f4429c12
Savitech .io <savitech1622@gmail.com>
2025-07-13 10:55:18 +0000

    Team Detail Page : Task Cards

---
+++ UI_PMS/src/component/BackupCard.jsx
@@ -0,0 +1,61 @@
+import { GiFiles } from "react-icons/gi";
+import { SiFiles } from "react-icons/si";
+import { FaCircle } from "react-icons/fa";
+import { FaAngleDown } from "react-icons/fa";
+import OrgCard from "../component/OrgCard";
+import { IoGrid } from "react-icons/io5";
+import { IoIosSettings } from "react-icons/io";
+import { MdSpaceDashboard } from "react-icons/md";
+import { RiTeamFill } from "react-icons/ri";
+import { CiSearch } from "react-icons/ci";
+import { useEffect, useState } from "react";
+import { useMyStore } from "../store/myStore";
+import { supabase } from "../../supabaseConfig";
+import { IoCloseSharp } from "react-icons/io5";
+import { FaInfoCircle } from "react-icons/fa";
+import { FaRegComment } from "react-icons/fa6";
+import { FaRegUserCircle } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
+import { useParams } from "react-router-dom";
+import { FaRegUser } from "react-icons/fa";
+import ProjectCard from "../component/ProjectCard";
+
+const BackupCard = () => {
+    return (
+    <div style={{boxShadow:'0 2px 12px rgba(0, 0, 0, 0.08)', width:'100%', display:'flex', marginBlock:'8px', gap:'48px', paddingInline:'32px', paddingBlock:'20px', border:'2px solid #00000016', borderRadius:'12px', alignItems:'center'}}>
+        <div>
+            <p style={{fontSize:'18px', fontWeight:500}}>🎉🐞🧪📝Login Page</p>
+            <div style={{color:'#00000070', display:'flex', alignItems:'center', gap:'4px', marginTop:'12px'}}>
+                <FaRegUser size={12}/>
+                <p style={{fontSize:12}}>Yash Raghuvanshi</p>
+            </div>
+        </div>
+        <div style={{display:'flex', gap:'12px', alignItems:'center'}}>
+            <div style={{display:'flex', gap:'12px', alignItems:'center'}}>
+            <p style={{fontSize:'12px', backgroundColor:'#FFC1C4', paddingInline:'6px', paddingBlock:'4px', color:'black', borderRadius:'6px', fontWeight:500}}>Not Started</p>
+            <p style={{fontSize:'12px', backgroundColor:'#EACEFE', paddingInline:'6px', paddingBlock:'4px', color:'black', borderRadius:'6px', fontWeight:500}}>In Progress</p>
+            <p style={{fontSize:'12px', backgroundColor:'#B2F2AD', paddingInline:'6px', paddingBlock:'4px', color:'black', borderRadius:'6px', fontWeight:500}}>Completed</p>
+            </div>
+
+            <div style={{display:'flex', gap:'12px', alignItems:'center'}}>
+            <p style={{fontSize:'12px', backgroundColor:'#EAEAEA', paddingInline:'6px', paddingBlock:'4px', color:'black', borderRadius:'6px', fontWeight:500}}>Not Review</p>
+            <p style={{fontSize:'12px', backgroundColor:'#FDECC9', paddingInline:'6px', paddingBlock:'4px', color:'black', borderRadius:'6px', fontWeight:500}}>Suggestions Given</p>
+            <p style={{fontSize:'12px', backgroundColor:'#C9DBFE', paddingInline:'6px', paddingBlock:'4px', color:'black', borderRadius:'6px', fontWeight:500}}>Perfect</p>
+            </div>
+
+            <div style={{fontSize:'12px', paddingInline:'6px', paddingBlock:'4px', color:'#00000080', borderRadius:'6px', display:'flex', alignItems:'center', gap: '4px', fontWeight:500, border:'2px solid #00000022'}}><FaRegComment /> <p style={{color:'black'}}>0</p></div>
+            
+            <div style={{display:'flex', gap:'12px', alignItems:'center'}}>
+            <div style={{width:'120px', position:'relative'}}>
+                <div style={{width:'100%', height:'6px', backgroundColor:'#00000036', borderRadius:'8px'}}></div>
+                <div style={{width:'20%', height:'6px',top:0, left:0, position:'absolute', zIndex:12, backgroundColor:'black', borderRadius:'8px'}}></div>
+            </div>
+            <p style={{color:'#00000085', fontSize:12}}>20%</p>
+            </div>
+
+        </div>
+    </div>
+    )
+}
+
+export default BackupCard
--- UI_PMS/src/pages/TeamDetails.jsx
+++ UI_PMS/src/pages/TeamDetails.jsx
@@ -13,9 +13,16 @@ import { useMyStore } from "../store/myStore";
 import { supabase } from "../../supabaseConfig";
 import { IoCloseSharp } from "react-icons/io5";
 import { FaInfoCircle } from "react-icons/fa";
+import { FaRegComment } from "react-icons/fa6";
+import { FaRegUserCircle } from "react-icons/fa";
 import { useNavigate } from "react-router-dom";
 import { useParams } from "react-router-dom";
+import { FaRegUser } from "react-icons/fa";
 import ProjectCard from "../component/ProjectCard";
+import TaskCard from "../component/TaskCard";
+import BugCard from "../component/BugCard";
+import TestCard from "../component/TestCard";
+import ReportCard from "../component/ReportCard";
 
 const TeamDetails = () => {
     let [orgList, setOrgList] = useState([]);
@@ -31,6 +38,7 @@ const TeamDetails = () => {
     const {userid} = useMyStore();
 
     let [projectDetails, setProjectDetails] = useState({});
+    let [guideDetails, setGuideDetails] = useState({});
     let [memberDetails, setMemberDetails] = useState([]);
 
     useEffect(() => {
@@ -49,7 +57,7 @@ const TeamDetails = () => {
 
         let { data: t_sys_teams, error } = await supabase
         .from('t_sys_teams')
-        .select(`*, user_team: userteam(id, users)`)
+        .select(`*, user_team: userteam(id, users), guide_info: guide(id, name)`)
         .eq('org_id', orgId).eq('id', teamId);
 
         if(error)
@@ -66,6 +74,13 @@ const TeamDetails = () => {
             name: project.project_name,
             status : project.status
           }
+
+          const guideDetailObj = {
+            id : project.guide_info.id,
+            name: project.guide_info.name
+          }
+
+          setGuideDetails(guideDetailObj);
 
           const members = project?.user_team?.users;
           const memberId = members.map((val, ind) => {
@@ -90,11 +105,11 @@ const TeamDetails = () => {
           {
             setMemberDetails([]);
           }
-
           setProjectDetails(projectDetailObj);
         }
         else
         {
+          setGuideDetails({});
           setProjectDetails({});
         }
 
@@ -111,12 +126,12 @@ const TeamDetails = () => {
     }
 
     return (
-        <div style={{width:'100%', height:'100vh'}}>
+        <div style={{width:'100%', height:'90vh', overflowY:'scroll'}}>
             <div style={{display:'flex', width:'100%'}}>
               {/* mainwindow */}
               <div style={{width:'100%'}}>
 
-                  <div style={{paddingInline:60, paddingTop:24}}>
+                  <div style={{paddingInline:60, paddingTop:24, borderBottom:'1px solid #00000016', paddingBottom:'24px'}}>
                     <div style={{display:'flex', gap:'8px', alignItems:'center'}}>
                         <h2 style={{fontSize:24}}>{projectDetails?.name || ''}</h2>
                         {projectDetails?.status == 1 && <p style={{fontSize:'12px', backgroundColor:'red', paddingInline:'6px', paddingBlock:'4px', color:'white', borderRadius:'12px', fontWeight:500}}>Not Started</p>}
@@ -125,7 +140,17 @@ const TeamDetails = () => {
                     </div>
                     <div style={{display:'flex', flexDirection:'column'}}>
                       <h2 style={{fontSize:16, color:'#00000070', fontWeight:400, marginTop:'8px'}}>Members</h2>
-                      <div style={{display:'flex', gap:'8px', marginTop:'6px'}}>
+                      <div style={{display:'flex', gap:'20px', marginTop:'6px'}}>
+                        {
+                           (guideDetails!= null && guideDetails?.name) && <>
+                                <div style={{display:'flex', alignItems:'center', flexDirection:'column'}}>
+                                  <div style={{marginBlock:'4px', boxShadow:'0 1px 4px rgba(0, 0, 0, 0.3)', fontSize:'10px', color:'white', background:'purple', fontWeight:600, width:'30px', height:'30px', border:'2px solid #ffffff', borderRadius:'120', display:'flex', justifyContent:'center', alignItems:'center', borderRadius:'120px'}}>
+                                    {guideDetails?.name?.split(' ').length > 1 ? guideDetails?.name?.split(' ')[0][0]?.toUpperCase()+guideDetails?.name?.split(' ')[1][0]?.toUpperCase(): guideDetails?.name[0]?.toUpperCase()}
+                                  </div>
+                                  <h2 style={{fontSize:12, fontWeight:400, textAlign:'center', color:'#00000092'}}>{guideDetails.name}</h2>
+                                </div>
+                              </>
+                        }
                         {
                           memberDetails?.map((val, ind) => {
                             return (
@@ -134,7 +159,7 @@ const TeamDetails = () => {
                                   <div style={{marginBlock:'4px', boxShadow:'0 1px 4px rgba(0, 0, 0, 0.3)', fontSize:'10px', color:'white', background:'purple', fontWeight:600, width:'30px', height:'30px', border:'2px solid #ffffff', borderRadius:'120', display:'flex', justifyContent:'center', alignItems:'center', borderRadius:'120px'}}>
                                     {val.name.split(' ').length > 1 ? val.name.split(' ')[0][0].toUpperCase()+val.name.split(' ')[1][0].toUpperCase(): val.name[0].toUpperCase()}
                                   </div>
-                                  <h2 style={{fontSize:12, fontWeight:400, textAlign:'center'}}>{val.name.split(' ')[0]}</h2>
+                                  <h2 style={{fontSize:12, fontWeight:400, textAlign:'center', color:'#00000092'}}>{val.name.split(' ')[0]}</h2>
                                 </div>
                               </>
                             )
@@ -142,6 +167,16 @@ const TeamDetails = () => {
                         }
                       </div>
                     </div>
+                  </div>
+
+                  <div style={{paddingInline:60, paddingTop:24}}>
+                      <h2 style={{fontSize:16, color:'#00000092'}}>Tasks: 10</h2>
+                      <div style={{marginTop: 12 }}>
+                        <TaskCard />
+                        <BugCard />
+                        <TestCard />
+                        <ReportCard />
+                      </div>
                   </div>
                   
 
